7a31b67f534615e1670f3e019a37df356c8319be
Ruslan Migal <rm.nonsense@gmail.com>
2016-07-19 16:49:03 +0300

    changing color of skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,12 @@
 class Skill < ActiveRecord::Base
   belongs_to :resume
   def change_color(color)
-    @darker_color = color
-    change = @darker_color[2,3].to_i
-    change += 20
-    @darker_color[2,3] = change.to_s
-    return @darker_color
+    red = color[1,2].to_i + 15
+    green1 = color[3]
+    green2 = color[4]
+    green = "#{green1}#{green2}".to_i + 15
+    blue = color[5,6].to_i + 15
+    rgb = "##{red}#{green}#{blue}"
+    return rgb
   end
 end
