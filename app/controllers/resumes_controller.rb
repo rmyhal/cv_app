5cb379956536f2e65161671489bd1fe2a08f795f
Ruslan Migal <rm.nonsense@gmail.com>
2016-07-20 16:02:17 +0300

    changed logo size

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -5,42 +5,44 @@ class ResumesController < ApplicationController
   def index
     @resumes = Resume.all
   end
+
   def show
     @resume = Resume.find(params[:id])
-
   end
+
   def new
     @resume = Resume.new
   end
+
   def edit
     @resume = Resume.find(params[:id])
   end
+
   def create
     @resume = Resume.new(resume_params)
 
-    flash[:notice] = "Resume created"
     if @resume.save
       redirect_to resume_path(@resume)
     else
       render 'new'
       end
   end
+
   def update
     @resume = Resume.find(params[:id])
 
     if @resume.update(resume_params)
-      flash[:notice] = "Resume updated"
       redirect_to resume_path(@resume)
     else
       render 'edit'
     end
   end
+
   def destroy
     @resume = Resume.find(params[:id])
     @resume.destroy
     redirect_to resumes_path
   end
-
 
   private
   def resume_params
